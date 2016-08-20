e2a96756e96340fbe9757f08956410941e8756c0
Izzy Ali <izzywizzz@gmail.com>
2016-08-20 16:46:53 +0100

    Initial Commit

---
+++ LittleKnownFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LittleKnownFacts
+//
+//  Created by Isfandyar Ali on 20/08/2016.
+//  Copyright © 2016 Isfandyar Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
